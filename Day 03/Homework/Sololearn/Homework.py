f3d657573a13f8ccf831de467e29955bc4f9e93f
Nikokaramovi <karamoviniko2@gmail.com>
2024-10-10 00:11:47 +0400

    fourth

---
+++ Day 03/Homework/Sololearn/Homework.py
@@ -0,0 +1,96 @@
+"""1)გააკეთეთ ხუთ ხუთი მაგალითი ყველა გავლილ მათემატიკურ ოპერატორზე."""
+#+
+operation1 = 7 + 3
+print(operation1)
+
+operation2  = 5 + 3
+print(operation2)
+
+operation3 = 15 + 13
+print(operation3)
+
+operation4 = 7 + 7
+print(operation4)
+
+operation5 = 4 + 10
+print(operation5)
+
+#-
+
+operation6 = 5 - 3
+print(operation6)
+
+operation7 = 18 - 16
+print(operation7)
+
+operation8 = 40 - 32
+print(operation8)
+
+operation9 = 67 - 45
+print(operation9)
+
+operation10 = 78 - 60
+print(operation10)
+
+#*
+
+operation11 = 8 * 8
+print(operation11)
+
+operation12 = 4 * 6
+print(operation12)
+
+operation13 = 13 * 12
+print(operation13)
+
+operation14 = 3 * 13
+print(operation14)
+
+operation15 = 47 * 78
+print(operation15)
+
+#/
+
+operation16 = 88 / 2
+print(operation16)
+
+operation17 = 68 / 2
+print(operation17)
+
+operation18 = 98 / 24
+print(operation18)
+
+operation19 = 35 / 7
+print(operation19)
+
+operation20 = 64 / 8
+print(operation20)
+
+
+
+"""2)შექმენით ცვლადი age და მნიშვნელობად გადაეცით თქვენი ასაკი, შექმენით ახალი ცვლადი სადაც იქნება თქვენი ასაკი 5 წლის შემდეგ, ბოლოს კი გამოიტანეთ ეს ცვლადი."""
+
+age = 13
+age_in_5_years_late = age + 5
+print(age_in_5_years_late)
+ 
+
+
+"""3)თქვენი ოჯახის წევრების ასაკები შეინახეთ ცვლადებში და დაითვალეთ ყველა ასაკის ჯამი."""
+
+mom_age = 35
+Dad_age = 40
+brother_age = 15
+sister_age = 8
+grandmother_age = 63
+grandfather_age = 61
+
+all_parents_age = mom_age + Dad_age + brother_age + sister_age + grandmother_age + grandfather_age
+print(all_parents_age)
+
+"""4)შექმენით ცვლადი name რომელშიც შეინახავთ თქვენ სახელს და შემდეგ განაახლეთ ამ ცვლადის მნიშვნელობა და ჩაწერეთ თქვენი მეგობრის სახელი, საბოლოოდ დაბეჭდეთ name ცვლადი"""
+
+
+name = "niko"
+name = "giorgi"
+print(name)
