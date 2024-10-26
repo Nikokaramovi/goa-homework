266b7469122436fc2470e68d0fd5c6797963844e
Nikokaramovi <karamoviniko2@gmail.com>
2024-10-26 16:12:47 +0400

    fifthcommit

---
+++ homework.py
@@ -0,0 +1,43 @@
+
+"""1)მომხმარებელს შემოატანინე თავისი სახელი, შემდეგ შემატანინეთ მეგობრის გვარი და დაბეჭდეთ ისინი კონკატინაციის მეშვეობით."""
+
+user_name = input("please enter your name :")
+friend_surname = input("please enter friend surname :")
+full_name = user_name + " " + friend_surname
+print(full_name)
+
+
+
+"""2)მომხმარებელს შემოატანინეთ მისი ასაკი და დაუბეჭდეთ რამდენი წლის იქნება 10 წელში."""
+
+age = 13
+age_in_10_years_late = age + 10
+print(age_in_10_years_late)
+
+
+"""3)მომხმარებელს შემოატანინეთ 5 რიცხვი და დაითვალეთ ამ რიცხვების საშუალო არითმეტიკული"""
+
+num1 = input(45)
+num2 = input(15)
+num3 = input(20)
+num4 = input(10)
+num5 = input(10)
+print(num1 + num2 + num3 + num4 + num5 /5)
+
+
+"""4)კომენტარის სახით ახსენით რა არის Case Sensitive + რა არის Snake Case."""
+
+# case sensitive არის მაგალითად: თუ სისტემაში არის სიტყვა "Book",   მაშინ "book" და "BOOK"  ვერ იქნება როცა არის case sensitive
+
+# Snake case (ან "snake_case") არის დასახელების კონვენცია, რომელიც შედგენილ სიტყვებში სივრცეებს ​​ცვლის ქვედა ხაზებით (_).
+
+
+
+"""5)დაუშვით 5 შეცდომა და გვერდით კომენტარის სახით მიუწერეთ სწორი ვერსია (ახსენით რა არის Debugging)"""
+
+1#print  = (name)   # სწორი პასუხი: print(name)
+2#color = orange  # სწორი პასუხი: color = "orange"
+3#name = giorgi  #სწორი პასუხი: name = "giorgi"
+4#print("book')   # სწორი პასუხი: print("book")
+5#age13 #სწორი პასუხი: age = 13
+
