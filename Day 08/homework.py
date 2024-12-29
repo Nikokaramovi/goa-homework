a1cfbb96aa63c1eb10b000b21017c74c45269997
Nikokaramovi <karamoviniko2@gmail.com>
2024-12-29 19:15:23 +0400

    nineth commit

---
+++ Day 08/homework.py
@@ -0,0 +1,146 @@
+#1
+print(5>3)
+print(3>4)
+print(10>3)
+
+print(5<3)
+print(3<4)
+print(10<3)
+
+num1 = int(input("Enter your number:" ))
+num2 = int(input("Enter your number:" ))
+print(num1 > num2)
+
+num3 = int(input("Enter your number:" ))
+num4 = int(input("Enter your number:" ))
+print(num3 > num4)
+
+num5 = int(input("Enter your number:" ))
+num6 = int(input("Enter your number:" ))
+print(num5 > num6)
+
+
+
+
+num7 = int(input("Enter your number:" ))
+num8 = int(input("Enter your number:" ))
+print(num7 >= num8)
+
+num9 = int(input("Enter your number:" ))
+num10 = int(input("Enter your number:" ))
+print(num9 >= num10)
+
+num11 = int(input("Enter your number:" ))
+num12 = int(input("Enter your number:" ))
+print(num11 >= num12)
+
+
+
+
+
+
+num01 = int(input("Enter your number:" ))
+num02 = int(input("Enter your number:" ))
+print(num02 <= num01)
+
+num03 = int(input("Enter your number:" ))
+num04 = int(input("Enter your number:" ))
+print(num03 <= num04)
+
+num05 = int(input("Enter your number:" ))
+num06 = int(input("Enter your number:" ))
+print(num06 <= num05)
+
+
+
+#2
+
+
+
+num01 = int(input("Enter your number:" ))
+num02 = int(input("  my number:" ))
+print(num02 == num01)
+
+
+
+
+
+
+
+
+
+
+
+
+num07 = int(input("Enter your number:" ))
+num08 = int(input("Enter your number:" ))
+print(num07 != num08)
+
+num09 = int(input("Enter your number:" ))
+num10 = int(input("Enter your number:" ))
+print(num09 != num10)
+
+num11 = int(input("Enter your number:" ))
+num12 = int(input("Enter your number:" ))
+print(num11 != num12)
+
+
+
+num001 = int(input("Enter your number:" ))
+num002 = int(input("Enter your number:" ))
+print(num001 == num002)
+
+num003 = int(input("Enter your number:" ))
+num004 = int(input("Enter your number:" ))
+print(num003 == num004)
+
+num005 = int(input("Enter your number:" ))
+num006 = int(input("Enter your number:" ))
+print(num005 == num006)
+
+
+#3
+
+
+
+True or False and 5 > 3 or "name" == "name" and 123 == "123" and 5 >= 5
+
+#4
+
+
+surname = int(input("Enter your surname:" ))
+age = int(input("Enter your age:" ))
+print(age == surname)
+
+
+
+#5 https://upload.wikimedia.org/wikipedia/commons/9/9d/Flowchart.png
+# 1ალგორითმის მაგალითი:მაგალითად ვაკეთებთ ტორტს და ტორტს აქვს რეცეპტი რომელსაც უნდა მივყვეთ და თუ არასწორად მივყევით არაფერი გამოგვივა.
+
+
+
+
+
+#7
+#Selection
+#sequecing
+#iteration
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
