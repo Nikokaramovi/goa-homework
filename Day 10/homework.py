45a028c489063fede8d73fc62b8e7e4e3b2a23ed
Nikokaramovi <karamoviniko2@gmail.com>
2025-01-13 19:07:55 +0400

    twelfth commit

---
+++ Day 10/homework.py
@@ -0,0 +1,40 @@
+#1completed
+
+
+
+
+#2
+for i in range(int(input("enter your number :"))):
+    print(i)
+
+
+#3
+ 
+for i in range(int(input("enter your number: ")),-1,-1):
+    print(i)
+
+
+#4
+
+
+i=0
+while i<30:
+    if i %2!=0:
+        print(i)
+    i+=1
+
+
+
+#5
+
+
+for i in range(30):
+    if i%2==0:
+        print(i)
+
+
+
+#6
+
+
+# 
